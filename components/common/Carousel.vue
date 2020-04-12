c615309abeda825eb68bb1694ebe08e5ff441535
Aito Hideki <aito.hideki@gmail.com>
2020-04-12 15:03:14 -0400

    infinite scroll logic embedded

--- components/common/Carousel.vue
+++ components/common/Carousel.vue
@@ -4,14 +4,21 @@
     v-bind="$attrs"
     class="common-carousel d-flex flex-row align-center justify-center"
   >
-    <V-btn icon>
+    <V-btn
+      icon
+      :disabled="!ablePrev"
+      @click="prev"
+    >
       <v-icon>mdi-chevron-left</v-icon>
     </V-btn>
     <div
       ref="wrapper"
       :style="wrapperStyle"
     >
-      <div class="common-carousel__content d-flex flex-row">
+      <div
+        class="common-carousel__content d-flex flex-row"
+        :style="contentStyle"
+      >
         <template v-for="(child, idx) in children">
           <div
             v-if="child.context"
@@ -23,8 +30,7 @@
           </div>
         </template>
         <div
-          v-if="infiniteLoading"
-          class="common-carousel__item"
+          class="common-carousel__item d-flex align-center justify-center"
           :style="itemStyle"
         >
           <v-progress-circular
@@ -36,45 +42,34 @@
         </div>
       </div>
     </div>
-    <V-btn icon>
+    <V-btn
+      icon
+      :disabled="!ableNext"
+      @click="next"
+    >
       <v-icon>mdi-chevron-right</v-icon>
     </V-btn>
   </div>
 </template>
 
 <script>
-import { ref, computed, onMounted, onBeforeUnmount } from '@vue/composition-api'
+import { ref, computed } from '@vue/composition-api'
 
-const useComponentStyling = (props) => {
+const useLayout = (props) => {
   const container = ref(null)
-  const wrapper = ref(null)
+  const containerWidth = computed(() => container.value ? container.value.clientWidth : 0)
 
-  const wrapperWidth = ref(0)
+  const itemsPerPage = computed(() => Math.floor((containerWidth.value - 88) / props.itemWidth))
+
+  const wrapper = ref(null)
+  const wrapperWidth = computed(() => ((props.maxPerPage && props.maxPerPage < itemsPerPage.value
+    ? props.maxPerPage : itemsPerPage.value) || 1) * props.itemWidth)
   const wrapperStyle = computed(() => ({
     overflow: 'hidden auto',
     width: `${wrapperWidth.value}px`
   }))
-  const itemsPerPage = ref(0)
 
-  const itemStyle = computed(() => ({
-    width: `${props.itemWidth}px`
-  }))
-
-  // Component resize event handler
-  const resizeHandlerTimer = ref(null)
-  const startResizeHandler = () => {
-    resizeHandlerTimer.value = setInterval(() => {
-      const width = container.value ? container.value.clientWidth : 0
-      itemsPerPage.value = Math.floor((width - 88) / props.itemWidth)
-      wrapperWidth.value = ((props.maxPerPage && props.maxPerPage < itemsPerPage.value ? props.maxPerPage : itemsPerPage.value) || 1) * props.itemWidth
-    }, 50)
-  }
-  const stopResizeHandler = () => {
-    clearInterval(resizeHandlerTimer.value)
-  }
-
-  onMounted(() => startResizeHandler())
-  onBeforeUnmount(() => stopResizeHandler())
+  const itemStyle = computed(() => ({ width: `${props.itemWidth}px` }))
 
   return {
     container,
@@ -83,6 +78,40 @@ const useComponentStyling = (props) => {
     itemStyle,
 
     itemsPerPage
+  }
+}
+
+const useCarouselIndex = (props, { itemsPerPage, children }) => {
+  const currentIdx = ref(0)
+  const reachableIndex = computed(() => {
+    const reachable = (children.value.length - itemsPerPage.value + (props.infiniteScroll ? 1 : 0)) || 0
+    return reachable > 0 ? reachable : 0
+  })
+  const pointerIdx = computed(() => {
+    const rcIdx = reachableIndex.value - (props.infiniteScroll && !props.loading ? 1 : 0)
+    const idx = currentIdx.value < rcIdx ? currentIdx.value : rcIdx
+    return idx >= 0 ? idx : 0
+  })
+
+  const ableNext = computed(() => currentIdx.value < reachableIndex.value)
+  const ablePrev = computed(() => currentIdx.value > 0)
+
+  const next = () => {
+    if (!ableNext.value) { return }
+    currentIdx.value = currentIdx.value + 1
+    console.log(currentIdx.value, reachableIndex.value, children.value.length, itemsPerPage.value)
+  }
+  const prev = () => {
+    if (!ablePrev.value) { return }
+    currentIdx.value = pointerIdx.value - 1
+  }
+
+  return {
+    pointerIdx,
+    ableNext,
+    ablePrev,
+    next,
+    prev
   }
 }
 
@@ -98,9 +127,13 @@ export default {
       type: Number,
       default: 250
     },
-    infiniteLoading: {
+    infiniteScroll: {
       type: Boolean,
-      defualt: false
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
     },
     maxPerPage: {
       type: Number,
@@ -108,13 +141,26 @@ export default {
     }
   },
   setup (props, { root, slots }) {
-    const componentStyle = useComponentStyling(props)
+    // Basic layout information
+    const componentStyle = useLayout(props)
+    const { itemsPerPage } = componentStyle
 
     // Get child items from default slot
     const children = computed(() => slots.default() ? slots.default().map(item => item) : [])
 
+    // Carousel Index management
+    const carouselIndex = useCarouselIndex(props, { itemsPerPage, children })
+    const { pointerIdx } = carouselIndex
+
+    // Content Box style
+    const contentStyle = computed(() => ({
+      'margin-left': `-${pointerIdx.value * props.itemWidth}px`
+    }))
+
     return {
       ...componentStyle,
+      ...carouselIndex,
+      contentStyle,
       children
     }
   }
@@ -127,6 +173,7 @@ export default {
     flex-grow: 0;
     align-items: center;
     margin-left: 0px;
+    transition: margin-left .5s;
   }
 
   &__item {
---
+++ pages/Profile.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-row no-gutters>
+    <v-col
+      cols="12"
+      lg="9"
+      xl="10"
+    >
+      <v-subheader>
+        <v-icon class="mr-1" color="secondary lighten-3">
+          mdi-heart
+        </v-icon>
+        3,483 Following
+      </v-subheader>
+      <common-carousel
+        v-for="i in 6"
+        :key="`content-${i}`"
+        :item-width="250"
+      >
+        <common-video-player
+          v-for="j in 6"
+          :key="`content-${i}-${j}`"
+          :options="playerOptions"
+        />
+      </common-carousel>
+    </v-col>
+    <v-col
+      cols="12"
+      lg="3"
+      xl="2"
+    >
+      <v-card>
+        <v-row no-gutters>
+          <v-spacer />
+          <v-subheader class="text-center">
+            ADS
+          </v-subheader>
+          <v-spacer />
+        </v-row>
+        <common-carousel
+          v-for="i in 6"
+          :key="`adv-${i}`"
+          :item-width="250"
+          :max-per-page="1"
+        >
+          <common-video-player
+            v-for="j in 6"
+            :key="`adv-${i}-${j}`"
+            :options="playerOptions"
+          />
+        </common-carousel>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // component options
+      playsinline: true,
+
+      // videojs options
+      playerOptions: {
+        muted: true,
+        language: 'en',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        fluid: true,
+        sources: [{
+          type: 'video/mp4',
+          src: 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm'
+        }]
+      }
+    }
+  }
+}
+</script>
