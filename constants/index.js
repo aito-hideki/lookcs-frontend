733bfa20afa479ed50ab80c838393adc79fa63fa
Aito Hideki <aito.hideki@gmail.com>
2020-05-05 21:36:02 -0400

    replaced the video

--- constants/index.js
+++ constants/index.js
@@ -8,8 +8,8 @@ export const playerOptions = {
   controls: true,
   sources: [{
     type: 'video/mp4',
-    // src: 'http://vjs.zencdn.net/v/oceans.mp4'
-    src: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4'
+    src: 'http://vjs.zencdn.net/v/oceans.mp4'
+    // src: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4'
   }],
   poster: '/thumbnails/tomjerry.jpg'
 }
